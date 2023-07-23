c156bec8fdee23ada636221aa74b974068ab24e0
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-23 16:48:39 +0300

    refactored code for DRY

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -9,11 +9,10 @@ function TransactionTable({
 }) {
   const handleDelete = (id) => {
     const remTransactions = transaction.filter((item) => id !== item.id);
-    // console.log(remTransactions);
     setTransaction(remTransactions);
   };
 
-  const filteredData = filtered.map((data) => {
+  const table = (data) => {
     return (
       <tr key={data.id} style={{ marginBottom: "1rem" }}>
         <td>{data.date}</td>
@@ -27,22 +26,38 @@ function TransactionTable({
         </td>
       </tr>
     );
+  };
+
+  const sort = (data, description) => {
+    let sorted = data.sort(function (a, b) {
+      if (a.desc < b.desc) {
+        return -1;
+      }
+      if (a.desc > b.desc) {
+        return 1;
+      }
+      return 0;
+    });
+  };
+  filtered = filtered.sort(function (a, b) {
+    if (a.description < b.description) {
+      return -1;
+    }
+    if (a.description > b.description) {
+      return 1;
+    }
+    return 0;
+  });
+
+  //   const filteredData2 = sort(filtered, data.category).map((data) => {
+  //     return table(data);
+  //   });
+  const filteredData = filtered.map((data) => {
+    return table(data);
   });
 
   const nonFilteredData = transaction.map((data) => {
-    return (
-      <tr key={data.id} style={{ marginBottom: "1rem" }}>
-        <td>{data.date}</td>
-        <td>{data.description}</td>
-        <td>{data.category}</td>
-        <td>{data.amount}</td>
-        <td>
-          <button className="delete-btn" onClick={() => handleDelete(data.id)}>
-            <FiTrash2 style={{ fontSize: "1.2rem" }} />
-          </button>
-        </td>
-      </tr>
-    );
+    return table(data);
   });
 
   return (
@@ -57,8 +72,11 @@ function TransactionTable({
             <th>Delete</th>
           </tr>
         </thead>
-        <tbody>{search.length > 1 ? filteredData : nonFilteredData}</tbody>
+        <tbody>{search.length > 0 ? filteredData : nonFilteredData}</tbody>
       </table>
+      <h3 className="error-search">
+        {filteredData.length < 1 && "Search query not found."}
+      </h3>
     </div>
   );
 }
