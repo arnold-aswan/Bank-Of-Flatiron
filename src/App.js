49b88e4e817503ccad3d31b159ea07e30fb463c1
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-23 15:18:02 +0300

    split code into their own functional components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { FiTrash2 } from "react-icons/fi";
+import SearchBar from "./components/SearchBar";
+import TransactionForm from "./components/TransactionForm";
+import TransactionTable from "./components/TransactionTable";
 import "./App.css";
 
 function App() {
@@ -19,155 +22,31 @@ function App() {
       .then((data) => setTransaction(data));
   }, []);
 
-  const handleDelete = (id) => {
-    const remTransactions = transaction.filter((item) => id !== item.id);
-    console.log(remTransactions);
-    setTransaction(remTransactions);
-  };
-
-  const handleChange = (search) => {
-    setSearch(search);
-    const updatedList = [...transaction];
-    if (search !== "") {
-      const filter = updatedList.filter(
-        (item) =>
-          // item.category.toLowerCase() ||
-          item.description.toLowerCase() === search
-      );
-      setFiltered(filter);
-    } else if (search === "") {
-      setFiltered(updatedList);
-    }
-  };
-
-  const handleFormData = (e) => {
-    const { name, value } = e.target;
-    const newValue = name === "amount" ? Number(value) : value;
-    setFormData({
-      ...formData,
-      [name]: newValue,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newTransaction = [...transaction, formData];
-    setTransaction(newTransaction);
-    // try {
-    //   fetch("http://localhost:3000/transactions", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(newTransaction),
-    //   })
-    //     .then((r) => r.json())
-    //     .then((data) => setTransaction(newTransaction));
-    // } catch (error) {
-    //   console.log(error + "something went wrong");
-    // }
-  };
-
-  const filteredData = filtered.map((trans) => {
-    return (
-      <tr key={trans.id} style={{ marginBottom: "1rem" }}>
-        <td>{trans.date}</td>
-        <td>{trans.description}</td>
-        <td>{trans.category}</td>
-        <td>{trans.amount}</td>
-        <td>
-          <button className="delete-btn" onClick={() => handleDelete(trans.id)}>
-            <FiTrash2 style={{ fontSize: "1.2rem" }} />
-          </button>
-        </td>
-      </tr>
-    );
-  });
-
-  const nonFilteredData = transaction.map((trans) => {
-    return (
-      <tr key={trans.id} style={{ marginBottom: "1rem" }}>
-        <td>{trans.date}</td>
-        <td>{trans.description}</td>
-        <td>{trans.category}</td>
-        <td>{trans.amount}</td>
-        <td>
-          <button className="delete-btn" onClick={() => handleDelete(trans.id)}>
-            <FiTrash2 style={{ fontSize: "1.2rem" }} />
-          </button>
-        </td>
-      </tr>
-    );
-  });
-
   return (
     <div className="App">
       <h1>FlatIron Bank</h1>
-      <input
-        className="search"
-        type="text"
-        onChange={(e) => handleChange(e.target.value)}
-        value={search}
-        placeholder="search description ..."
+
+      <SearchBar
+        setFiltered={setFiltered}
+        search={search}
+        setSearch={setSearch}
+        transaction={transaction}
       />
-      <h1>{search}</h1>
 
-      <form onSubmit={handleSubmit}>
-        <div className="form-control">
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            name="date"
-            value={formData.date}
-            onChange={handleFormData}
-          />
-        </div>
+      <TransactionForm
+        setFormData={setFormData}
+        formData={formData}
+        transaction={transaction}
+        setTransaction={setTransaction}
+      />
 
-        <div className="form-control">
-          <label htmlFor="description">Description</label>
-          <input
-            type="text"
-            name="description"
-            placeholder="description..."
-            value={formData.description}
-            onChange={handleFormData}
-          />
-        </div>
-        <div className="form-control">
-          <label htmlFor="category">Category</label>
-          <input
-            type="text"
-            name="category"
-            value={formData.category}
-            onChange={handleFormData}
-          />
-        </div>
-
-        <div className="form-control">
-          <label htmlFor="amount">Amount</label>
-          <input
-            className="amount"
-            type="number"
-            name="amount"
-            value={formData.amount}
-            onChange={handleFormData}
-          />
-        </div>
-        <input className="btn" type="submit" />
-      </form>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Category</th>
-            <th>Amount</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>{search.length > 1 ? filteredData : nonFilteredData}</tbody>
-      </table>
+      <TransactionTable
+        search={search}
+        transaction={transaction}
+        setTransaction={setTransaction}
+        filtered={filtered}
+        FiTrash2={FiTrash2}
+      />
     </div>
   );
 }
